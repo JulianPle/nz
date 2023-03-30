ffd71440843aa6e9ffd99d81983e3ea84007eba8
JulianPle <julian.plepla@gmx.de>
2023-03-30 15:52:30 +0200

    zoom und title variable eingeführt

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 let stop_lat = -44.616667;
 let stop_lng = 167.866667;
-let map = L.map('map').setView([stop_lat, stop_lng], 13);
+let zoom = 13;
+let title = 'Milford Sound';
+let map = L.map('map').setView([stop_lat, stop_lng], zoom);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 L.marker([stop_lat, stop_lng]).addTo(map)
-    .bindPopup('Milford Sound')
+    .bindPopup(title)
     .openPopup();
