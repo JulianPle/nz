e4305a1ef2b9ab08ba9186297a3b96af767e2d16
JulianPle <julian.plepla@gmx.de>
2023-03-30 16:07:36 +0200

    array stops definiert und angezeigt

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ let stop_lat = -44.616667;
 let stop_lng = 167.866667;
 let zoom = 13;
 let title = 'Milford Sound';
+
+const STOP = ["Moeraki Boulder", 'Milford Sound', 'Wanaka'];
+console.log(STOP);
+console.log(STOP[0]);
+console.log(STOP[1]);
+console.log(STOP[2]);
+console.log(STOP.length);
+
 let map = L.map('map').setView([stop_lat, stop_lng], zoom);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
